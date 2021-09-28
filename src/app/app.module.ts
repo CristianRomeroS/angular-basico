3d3d766bc39aaec6f66d38d89ffc606a8fccf506
Cristian Romero Siacara <cristian.romero56@gmail.com>
2021-09-28 13:33:50 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//tarea:
+//cread modulo condadormodule
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+     AppComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
+    //se importan los modulos aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
